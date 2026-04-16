1da36e72e8736b25d19a971f2a11d45e964b7255
Jose Valduz <jose.valduz@unet.edu.ve>
2026-04-16 12:46:48 -0400

    Exponer la ruta para crear libros se modifica en la api/libros y se crea el create_libro

--- api/libros.py
+++ api/libros.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from typing import List
 
 from database import get_db
-from services.libro_service import listar_libros
-from schemas import LibroRead
+from services.libro_service import listar_libros, crear_libro
+from schemas import LibroRead, LibroCreate
 
 # Router principal para libros
 router = APIRouter(
@@ -21,7 +21,19 @@ def get_libros(db: Session = Depends(get_db)):
     """
     Devuelve todos los libros registrados en la base de datos.
     """
+    return listar_libros(db)
 
-    libros = listar_libros(db)
 
-    return libros
+# ---------------------------------------------
+# ENDPOINT: CREAR LIBRO
+# ---------------------------------------------
+@router.post(
+    "/",
+    response_model=LibroRead,
+    status_code=status.HTTP_201_CREATED
+)
+def create_libro(libro: LibroCreate, db: Session = Depends(get_db)):
+    """
+    Crea un nuevo libro en la base de datos.
+    """
+    return crear_libro(db, libro)
