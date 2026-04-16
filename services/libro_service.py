ad46ea69fc8ecae460f2a1147339fa7a403c6b87
Jose Valduz <jose.valduz@unet.edu.ve>
2026-04-16 12:41:32 -0400

    Incluye la logica para crear un libro con pydantic en la BD

--- services/libro_service.py
+++ services/libro_service.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from models.libro import Libro
+from schemas import LibroCreate
 
 
 # ---------------------------------------------
@@ -8,11 +9,33 @@ from models.libro import Libro
 def listar_libros(db: Session):
     """
     Obtiene todos los libros de la base de datos.
-
-    Parámetros:
-        db (Session): sesión activa de SQLAlchemy
-
-    Retorna:
-        Lista de objetos Libro
     """
     return db.query(Libro).all()
+
+
+# ---------------------------------------------
+# SERVICIO: CREAR LIBRO
+# ---------------------------------------------
+def crear_libro(db: Session, datos: LibroCreate):
+    """
+    Crea un nuevo libro en la base de datos.
+
+    Parámetros:
+        db: sesión de base de datos
+        datos: información validada del libro (Pydantic)
+
+    Retorna:
+        Libro creado
+    """
+
+    nuevo_libro = Libro(
+        titulo=datos.titulo,
+        autor=datos.autor,
+        rating=datos.rating
+    )
+
+    db.add(nuevo_libro)
+    db.commit()
+    db.refresh(nuevo_libro)
+
+    return nuevo_libro
