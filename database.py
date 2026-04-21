0e7d84d8f2cd358d8286a17c7695a3499d790ecc
Jose Valduz <jose.valduz@unet.edu.ve>
2026-04-21 11:50:15 -0400

    deploy render postgres

--- database.py
+++ database.py
@@ -14,7 +14,7 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
 # Construir URL de conexión
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Crear engine (conexión base)
 engine = create_engine(
